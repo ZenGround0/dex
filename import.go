3522e4885906695816689f288fd018aa500d6102
Wyatt Daviau <wdaviau@cs.stanford.edu>
2018-02-02 13:26:12 -0500

    moving from printing blockstore to dagservice

--- import.go
+++ import.go
@@ -5,34 +5,23 @@ import (
 	"strings"
 
 	"github.com/ipfs/go-ipfs-cmdkit/files"
-	bstore "github.com/ipfs/go-ipfs/blocks/blockstore"
-	blockservice "github.com/ipfs/go-ipfs/blockservice"
 	"github.com/ipfs/go-ipfs/core/coreunix"
-	"github.com/ipfs/go-ipfs/exchange/offline"
-	dag "github.com/ipfs/go-ipfs/merkledag"
 )
 
 // ImportToPrint is a first step towards a streaming importer.  Verify that we
 // can hijack the blockstore abstraction to redirect blocks as they arrive
 // Closely follows go-ipfs/core/commands/add.go: Run func
 func ImportToPrint(file files.File) error {
-	// Init objects needed by adder
-	pbs := &Pblockstore{
+	dserv := &pDAGService{
 		membership: make(map[string]bool),
-	} // This "stores" blocks by printing them to stdout
-	nopBs := &nopBlockstore{} // This "stores" blocks by doing nothing
-	locker := bstore.NewGCLocker()
-	addblockstore := bstore.NewGCBlockstore(pbs, locker)
-	exchblockstore := bstore.NewGCBlockstore(nopBs, locker)
+	}
 
-	exch := offline.Exchange(exchblockstore)
-	bserv := blockservice.New(addblockstore, exch)
-	dserv := dag.NewDAGService(bserv)
 	ctx := context.Background() // using background for now, should upgrade later
-	fileAdder, err := coreunix.NewAdder(ctx, nil, addblockstore, dserv)
+	fileAdder, err := coreunix.NewAdder(ctx, nil, nil, dserv)
 	if err != nil {
 		return err
 	}
+	fileAdder.Pin = false // This way we can be honest that blockstore doesn't exist
 
 	// add the file
 	if err := fileAdder.AddFile(file); err != nil {
@@ -44,7 +33,7 @@ func ImportToPrint(file files.File) error {
 
 	_, err = fileAdder.Finalize()
 	// ignore errors caused by printing-blockstore Get not finding blocks
-	if !strings.Contains(err.Error(), "blockstore: block not found") {
+	if !strings.Contains(err.Error(), "dagservice: block not found") {
 		return err
 	}
 	return nil
